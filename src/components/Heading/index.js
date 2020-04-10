bfaee0a798eb274c0c014cb2007cb8e659a1cf70
huguantao <huguantao@kuaishou.com>
2020-04-10 13:50:59 +0800

    fix: exit h5

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -3,12 +3,13 @@
 * @author: huguantao
 * @Date: 2020-02-29 19:57:23
 * @LastEditors: huguantao
-* @LastEditTime: 2020-04-09 15:07:35
+* @LastEditTime: 2020-04-10 13:08:37
  */
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { GoBackBlack } from '../../assets/image/assetsImages';
 import './index.scss';
+import Toast from '../Toast/Toast';
 
 function Heading(prop) {
   const [title, setTitle] = useState('');
@@ -23,6 +24,7 @@ function Heading(prop) {
   const goBack = () => {
     if(prop.type && prop.type === 'exit') {
       // 退出h5
+      Toast.show({mess: '点击左上角返回按钮,触发退出h5'})
       window.ToPayJSBridge.invoke('leaveWeb');
     } else {
       history.goBack();
