e04cd0d5414465cafaf04c3eba5bf6f477b03680
huguantao <huguantao@kuaishou.com>
2020-04-17 00:01:52 +0800

    fix: pull refresh

--- src/pages/UsingDetail.js
+++ src/pages/UsingDetail.js
@@ -3,13 +3,15 @@
 * @author: huguantao
 * @Date: 2020-03-26 11:46:07
 * @LastEditors: huguantao
-* @LastEditTime: 2020-04-13 22:02:29
+* @LastEditTime: 2020-04-17 00:00:50
  */
 import React, {useState, useEffect} from 'react';
 import { useHistory } from 'react-router-dom';
 import {request} from '../utils/request';
 import {GoBackWhite, UsingDetail_banner, UsingDetail_top, Tip} from '../assets/image/assetsImages';
 import '../styles/usingDetail.scss';
+
+let startTime = null;
 
 function UsingDetail() {
   const [orderData, setOrderData] = useState({
@@ -27,20 +29,28 @@ function UsingDetail() {
       checkStatus();
     }, 30000);
 
-    // 监听touchend事件，下拉刷新
-    let parent = document.querySelector('.using-detail-page');
-    parent.addEventListener('touchend', () => { 
-      if(isTop()){
-        checkStatus();
-      }
-      return;
-    })
+    // 监听touch事件，下拉刷新
+    let parent = document.querySelector('.touch-zone');
+    parent.addEventListener('touchstart', touchStart);
+    parent.addEventListener('touchend', touchEnd);
 
     return ()=> {
       clearInterval(intervalCheck);
-      parent.removeEventListener('touchend', () => { });
+      parent.removeEventListener('touchstart', touchStart);
+      parent.removeEventListener('touchend', touchEnd);
     }
   }, []);
+
+  const touchStart = () => {
+    startTime = new Date().getTime();
+  }
+  const touchEnd = () => {
+    // 页面刷到顶且touch超过500ms 刷新
+    if(isTop() && (new Date().getTime() - startTime > 500)){
+      checkStatus();
+    }
+    return;
+  }
 
   const checkStatus = () => {
     const headers = {
@@ -90,40 +100,42 @@ function UsingDetail() {
         <img src={UsingDetail_banner} alt="banner" className="banner" />
         <img src={GoBackWhite} alt="back" className="back" onClick={goback} />
       </div>
-      <h3 className='fonts desc font-24'>In Use ...</h3>
-      <p className='fonts title font-13'>Please return the powerbank to a nearby station after you finished.</p>
-      <div className="contents">
-        <div className='head'>
-          <span className='font-20'>Usage</span>
-          <img src={UsingDetail_top} alt="top" />
+      <div className='touch-zone'>
+        <h3 className='fonts desc font-24'>In Use ...</h3>
+        <p className='fonts title font-13'>Please return the powerbank to a nearby station after you finished.</p>
+        <div className="contents">
+          <div className='head'>
+            <span className='font-20'>Usage</span>
+            <img src={UsingDetail_top} alt="top" />
+          </div>
+          <div className="content">
+            <div className="half-content">
+              <p className="title font-13">Duration</p>
+              <p className="desc font-13">{orderData.duration}</p>
+            </div>
+            <div className="half-content">
+              <p className="title font-13">Amount</p>
+              <p className="desc font-13">AED {orderData.amount}</p>
+            </div>
+            <div className="half-content">
+              <p className="title font-13">Station</p>
+              <p className="desc font-13">{orderData.borrowStation && orderData.borrowStation.address}</p>
+            </div>
+            <div className="full-content">
+              <p className="title font-13">Pricing</p>
+              <p className="desc font-13">{orderData.chargingInfo && orderData.chargingInfo.title}</p>
+              <p className="desc font-13">{orderData.chargingInfo && orderData.chargingInfo.description}</p>
+            </div>
+          </div>
         </div>
-        <div className="content">
-          <div className="half-content">
-            <p className="title font-13">Duration</p>
-            <p className="desc font-13">{orderData.duration}</p>
-          </div>
-          <div className="half-content">
-            <p className="title font-13">Amount</p>
-            <p className="desc font-13">AED {orderData.amount}</p>
-          </div>
-          <div className="half-content">
-            <p className="title font-13">Station</p>
-            <p className="desc font-13">{orderData.borrowStation && orderData.borrowStation.address}</p>
-          </div>
-          <div className="full-content">
-            <p className="title font-13">Pricing</p>
-            <p className="desc font-13">{orderData.chargingInfo && orderData.chargingInfo.title}</p>
-            <p className="desc font-13">{orderData.chargingInfo && orderData.chargingInfo.description}</p>
-          </div>
-        </div>
-      </div>
 
-      <p className='font-13 red descs'><img src={Tip} alt='tip' />How to return a powerbank</p>
-      <p className='font-13 normal descs'>1. Find a station in the app with an empty returning slot.</p>
-      <p className='font-13 normal descs'>2. Plug the cables back to the powerbank and place the powerbank back to the staion with the arrow signs facing forward. A tone will ring in 3 seconds if the return is successful.</p>
-      <p className='font-13 normal descs'>3. If you encountered any issue, please contact our customer service for aid.</p>
-    
-      <p className='bottom-service font-13 text-center'>Customer service</p>
+        <p className='font-13 red descs'><img src={Tip} alt='tip' />How to return a powerbank</p>
+        <p className='font-13 normal descs'>1. Find a station in the app with an empty returning slot.</p>
+        <p className='font-13 normal descs'>2. Plug the cables back to the powerbank and place the powerbank back to the staion with the arrow signs facing forward. A tone will ring in 3 seconds if the return is successful.</p>
+        <p className='font-13 normal descs'>3. If you encountered any issue, please contact our customer service for aid.</p>
+      
+        <p className='bottom-service font-13 text-center'>Customer service</p>
+      </div>
     </div>
   );
 }
