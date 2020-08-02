e95bf98242fc776a9f7d798b552af23ad9108c6c
李栋 <ld167922@alibaba-inc.com>
2020-08-02 13:38:12 +0800

    feat: 去掉vconsole

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,8 +13,8 @@ import {getQueryString} from '../utils/helper';
 import '../styles/home.scss';
 import { Home_bg, Home_my, Home_exit, Home_scan, Home_using, Home_toPay } from '../assets/image/assetsImages';
 
-import VConsole from 'vconsole';
-var vConsole = new VConsole();
+// import VConsole from 'vconsole';
+// var vConsole = new VConsole();
 
 const msgs = [{
   img: Home_using,
