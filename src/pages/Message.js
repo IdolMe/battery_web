d242308cf091c622f90ac7d2c7817baf04ff674b
wangye <wangye@zodme.com>
2020-08-25 16:51:44 +0800

    fix order and pay

--- src/pages/Message.js
+++ src/pages/Message.js
@@ -5,24 +5,77 @@
 * @LastEditors: huguantao
 * @LastEditTime: 2020-05-07 22:48:18
  */
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Heading from '../components/Heading';
-import {request} from '../utils/request';
+import { request } from '../utils/request';
+import { PullToRefresh, ListView, Button } from 'antd-mobile';
+import ReactDOM from 'react-dom';
 import '../styles/message.scss';
 
+const DEFAULT_PAGESIZE = 10;
+
 function Message() {
-  const [messages, setMessages] = useState();
-  useEffect(() => {
+  const [data, setData] = useState([]);
+  const [dataSource, setDataSource] = useState(new ListView.DataSource({
+    rowHasChanged: (row1, row2) => row1 !== row2,
+  }));
+  const [isLoading, setIsLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const [pageNo, setPageNo] = useState(1);
+  const [total, setTotal] = useState(0);
+
+  const getMessageList = async (pageNo, pageSize = 10) => {
     const headers = {
       'userToken': sessionStorage.getItem('USERTOKEN'),
       'client-platform': 'WEB',
     };
-    request(`/v1.0.0/messages?pageIndex=1&pageSize=999`, 'GET', {}, headers ).then(res=> {
-      if(res.httpStatusCode === 200) {
-        setMessages(res.data)
-      }
-    })
+    const res = await request(`/v1.0.0/messages?pageIndex=${pageNo}&pageSize=${pageSize}`, 'GET', {}, headers);
+    if (res.httpStatusCode === 200) {
+      const prevData = pageNo === 1 ? [] : data;
+      const nextData = [...prevData, ...res.data.list];
+      setData(nextData);
+      setDataSource(dataSource.cloneWithRows(nextData));
+      setIsLoading(false);
+      setRefreshing(false);
+      setTotal(res.data.totalItems);
+    }
+  }
+
+  const onEndReached = () => {
+    if (isLoading || total <= pageNo * DEFAULT_PAGESIZE) {
+      return;
+    }
+    setIsLoading(true);
+    const nextPageNo = pageNo + 1;
+    setPageNo(nextPageNo);
+    getMessageList(nextPageNo, DEFAULT_PAGESIZE, true);
+  }
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setPageNo(1);
+    getMessageList(1, DEFAULT_PAGESIZE);
+  }
+
+  useEffect(() => {
+    getMessageList(1, DEFAULT_PAGESIZE);
   }, [])
+
+  // useEffect(() => {
+  //   document.documentElement.style.overflow = 'hidden';
+  // });
+
+  const row = (rowData, sectionID, rowID) => {
+    return (
+      <div className='message font-14 radius4' key={rowID}>
+        <div className='messageTitle'>
+          <span>{rowData.title}</span>
+          <span>{rowData.createTimestamp}</span>
+        </div>
+        <div className='desc'>{rowData.content}</div>
+      </div>
+    );
+  };
 
   return (
     <div className="message-page">
@@ -30,19 +83,30 @@ function Message() {
         <Heading title='Message' />
       </div>
       <div className='messages'>
-        <h3 className='font-24 title' />
         {
-          messages && messages.list && messages.list.length > 0 ? (
-            messages.list.map((item, index) => {
-              return <div className='message font-14 radius4' key={index}>
-                <div className='messageTitle'>
-                  <span>{item.title}</span>
-                  <span>{item.createTimestamp}</span>
-                </div>
-            <div className='desc'>{item.content}</div>
-              </div>
-            })
-          ) : <p className='font-14 title'>no message</p>
+          data && data.length ?
+            <ListView
+              dataSource={dataSource}
+              renderFooter={() => (<div style={{ padding: 30, textAlign: 'center' }}>
+                {isLoading ? 'Loading...' : 'Loaded'}
+              </div>)}
+              renderRow={row}
+              useBodyScroll={true}
+              pullToRefresh={<PullToRefresh
+                refreshing={refreshing}
+                onRefresh={onRefresh}
+                indicator={{
+                  activate: null,
+                  finish: null,
+                  deactivate: null,
+                  release: null,
+                }}
+              />}
+              onEndReachedThreshold={30}
+              onEndReached={onEndReached}
+              pageSize={10}
+            /> :
+            <p className='font-14 title'>no message</p>
         }
       </div>
     </div>
--- src/pages/OrderList.js
+++ src/pages/OrderList.js
@@ -7,28 +7,80 @@
  */
 import React, {useState, useEffect} from 'react';
 import { useHistory } from 'react-router-dom';
-import { Tabs } from 'antd';
+import { Tabs } from 'antd-mobile';
 import Heading from '../components/Heading';
 import {request} from '../utils/request';
 import {Checked} from '../assets/image/assetsImages';
+import { PullToRefresh, ListView, Button } from 'antd-mobile';
 import '../styles/orderList.scss';
+
+const DEFAULT_PAGESIZE = 10;
 
 function OrderList() {
   let history = useHistory();
-  const [orders, setOrders] = useState({});
+  const [data, setData] = useState([]);
+  const [dataSource, setDataSource] = useState(new ListView.DataSource({
+    rowHasChanged: (row1, row2) => row1 !== row2,
+  }));
+  const [isLoading, setIsLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const [pageNo, setPageNo] = useState(1);
+  const [total, setTotal] = useState(0);
 
-  useEffect(() => {
+  const getOrderList = async (pageNo, pageSize = 10) => {
     const headers = {
       'userToken': sessionStorage.getItem('USERTOKEN'),
+      'client-platform': 'WEB',
     };
-    // 'type': 'RENT'  // DEPOSIT RENT TOPUP   先不做分页
-    request(`/v1.0.0/orders?pageIndex=1&pageSize=999&type=RENT`, 'GET', {}, headers ).then(res=> {
-      if(res.httpStatusCode === 200) {
-        setOrders(res.data)
-      }
-    })
+    const res = await request(`/v1.0.0/orders?pageIndex=${pageNo}&pageSize=${pageSize}&type=RENT`, 'GET', {}, headers);
+    if (res.httpStatusCode === 200) {
+      const prevData = pageNo === 1 ? [] : data;
+      const nextData = [...prevData, ...res.data.list];
+      setData(nextData);
+      setDataSource(dataSource.cloneWithRows(nextData));
+      setIsLoading(false);
+      setRefreshing(false);
+      setTotal(res.data.totalItems);
+    }
+  }
+
+  const onEndReached = () => {
+    if (isLoading || total <= pageNo * DEFAULT_PAGESIZE) {
+      return;
+    }
+    setIsLoading(true);
+    const nextPageNo = pageNo + 1;
+    setPageNo(nextPageNo);
+    getOrderList(nextPageNo, DEFAULT_PAGESIZE);
+  }
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setPageNo(1);
+    getOrderList(1, DEFAULT_PAGESIZE);
+  }
+
+  useEffect(() => {
+    getOrderList(1, DEFAULT_PAGESIZE);
   }, [])
-  
+
+  const row = (item, sectionID, rowID) => {
+    return (
+      <div className='order radius4' key={rowID} onClick={()=>detail(item.orderNumber)}>
+        <div className='head'>
+          <span className='font-14'><img src={Checked} alt='check' />{item.borrowStatus}</span>
+          <span className='font-14'>AED {item.amount}</span>
+        </div>
+        <div className='content font-14'>
+          <p>Start time：{item.type == 'RENT' ? item.borrowStartTime : item.createTimestamp}</p>
+          {item.type == 'RENT' ? <p>Station：{item.borrowStation && item.borrowStation.address}</p> : null }
+          <p>Order Number：{item.orderNumber}</p>
+          <div className='detail font-12 text-center'>Details</div>
+        </div>
+      </div>
+    );
+  };
+
   const { TabPane } = Tabs;
   const callback = (key) => {
     console.log(key);
@@ -42,36 +94,35 @@ function OrderList() {
     <div className="order-list-page">
       <div className='header-wrap'>
         <Heading title='Orders' />
-        <div className='navs-wrap'>
-          <Tabs defaultActiveKey="1" onChange={callback}>
-            <TabPane tab="Powerbank" key="1" className='tab-bars'>
-              {/* Content of Tab Pane 1 */}
-            </TabPane>
-            {/* <TabPane tab="座充" key="2"></TabPane>
-            <TabPane tab="购买充电宝" key="3"></TabPane> */}
-          </Tabs>
-        </div>
       </div>
 
       <div className='lists'>
         {
-          orders.list && orders.list.length > 0 ? (
-            orders.list.map((item, index) => {
-              return <div className='order radius4' key={index} onClick={()=>detail(item.orderNumber)}>
-                <div className='head'>
-                  <span className='font-14'><img src={Checked} alt='check' />{item.borrowStatus}</span>
-                  <span className='font-14'>AED {item.amount}</span>
-                </div>
-                <div className='content font-14'>
-                  <p>Start time：{item.type == 'RENT' ? item.borrowStartTime : item.createTimestamp}</p>
-                  {item.type == 'RENT' ? <p>Station：{item.borrowStation && item.borrowStation.address}</p> : null }
-                  <p>Order Number：{item.orderNumber}</p>
-                  <div className='detail font-12 text-center'>Details</div>
-                </div>
-              </div>
-            })
-          ) : <p className='font-14 text-center'>no orders</p>
+          data && data.length ?
+            <ListView
+              dataSource={dataSource}
+              renderFooter={() => (<div style={{ padding: 30, textAlign: 'center' }}>
+                {isLoading ? 'Loading...' : 'Loaded'}
+              </div>)}
+              renderRow={row}
+              useBodyScroll={true}
+              pullToRefresh={<PullToRefresh
+                refreshing={refreshing}
+                onRefresh={onRefresh}
+                indicator={{
+                  activate: null,
+                  finish: null,
+                  deactivate: null,
+                  release: null,
+                }}
+              />}
+              onEndReachedThreshold={30}
+              onEndReached={onEndReached}
+              pageSize={10}
+            /> :
+            <p className='font-14 title'>no order</p>
         }
+
       </div>
     </div>
   );
